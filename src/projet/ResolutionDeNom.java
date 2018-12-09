05b8f64ee08ecf73f7943ad77c388a595fd9059f
crypi <cry.pierre@outlook.fr>
2018-12-09 14:41:31 +0100

    ResolutionDeNom

---
+++ src/projet/ResolutionDeNom.java
@@ -0,0 +1,22 @@
+package projet;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class ResolutionDeNom {
+
+	public static void main(String[] args) {
+
+		InetAddress address;
+		
+		try {
+			address = InetAddress.getLocalHost();
+			System.out.println("Nom : " + address.getHostName());
+			System.out.println("Adresse IP : " + address.getHostAddress());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
