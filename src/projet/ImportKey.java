09640c81711df87b071370b831745e86316f7701
crypi <cry.pierre@outlook.fr>
2018-12-09 16:25:52 +0100

    Method getKey in ImportKey

--- src/projet/ImportKey.java
+++ src/projet/ImportKey.java
@@ -14,15 +14,19 @@ public class ImportKey {
 		try {
 			Scanner scan = new Scanner(new File("mykey.txt"));
 			key = scan.next();
-			System.out.println(key);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
 	}
+	
+	public String getKey(){
+		return this.key;
+	}
 
 	public static void main(String[] args) {
 		ImportKey ik = new ImportKey();
+		System.out.println(ik.getKey());
 	}
 
 }
--- src/projet/ServeurTCP.java
+++ src/projet/ServeurTCP.java
@@ -19,6 +19,8 @@ public class ServeurTCP {
 		PrintWriter out;
 		String msg;
 		
+		
+		
 		final Scanner sc = new Scanner(System.in);
 		
 		// Création du Serveur
